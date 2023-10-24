41eb0691607215c125667f3f6a34da6a290a0d77
Renan Claude <renanclaude.rcg@gmail.com>
2023-10-24 20:06:06 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> user = this.userRepository.findById(id);
 		return user.get();
 	}
+
+	public User insert(User user) {
+		return userRepository.save(user);
+	}
 }
