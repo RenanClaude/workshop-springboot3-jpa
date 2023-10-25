c7c6b76606424ff3ed1c3a929192b40349d564eb
Renan Claude <renanclaude.rcg@gmail.com>
2023-10-25 00:29:15 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		this.userRepository.deleteById(id);
 	}
+
+	public User update(Long id, User obj) {
+		User entity = this.userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
+
 }
